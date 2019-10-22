ba075e64ac91840386a508eb1b70786e6b91fd3f
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-10-22 19:29:23 +0500

    Create Addition.cpp

---
+++ Addition.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int a, b, sum;
+	cout << "A <<< ";
+	cin >> a;
+	cout << "B <<< ";
+	cin >> b;
+	sum = a+b;
+	cout << sum;
+	return 0;
+}
