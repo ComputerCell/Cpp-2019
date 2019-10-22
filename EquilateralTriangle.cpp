493e35da9eb0411c7323e7a44345029897595de1
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-10-22 20:24:19 +0500

    Create EquilateralTriangle.cpp

---
+++ EquilateralTriangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 10; i++)
+	{
+		// Left side
+		for (int j = 0; j < 10-i; j++)
+		{
+			cout << " ";
+		}
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		
+
+		// Right Side
+		for (int j = 0; j < i; j++)
+		{
+			cout << "*";
+		}
+		cout << endl;
+	}
+
+	return 0;
+}
