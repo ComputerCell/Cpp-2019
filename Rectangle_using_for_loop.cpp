25346b23b0be24bd281d3c737abe7a2c99a151eb
QuantumNovice <43876848+QuantumNovice@users.noreply.github.com>
2019-10-22 19:34:01 +0500

    Create Rectangle_using_for_loop.cpp

---
+++ Rectangle_using_for_loop.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	for (int i = 0; i < 9; i++) {
+		cout << "*";
+	}
+	cout << endl;
+	for (int i = 0; i < 4; i++) {
+		cout << "*       *" << endl;
+	}
+	for (int i = 0; i < 9; i++) {
+		cout << "*";
+		
+	}
+	return 0;
+}
